a3b3930e71e0772755574754706fab507fc000f2
Muqto <muqto14@gmail.com>
2022-07-13 17:50:32 -0400

    2nd commit

---
+++ src/components/App.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+const API_BASE = "http://localhost:4000"
+function App() {
+  const [note, setNote] = useState([]);
+////////////////////////////////////////////////Connecting to backend////////////////////////////////////////////////////////////  
+  useEffect(() => {
+    fetch(API_BASE + "/notes")
+    .then((res) => res.json())
+    .then((data) => {setNote(data)})
+    .catch((err) => console.error("Error: ", err));
+    
+  }, [])
+
+  async function deleteNote(id){
+    const data = await fetch(API_BASE + `/note/delete/${id}`, {method:"DELETE"})
+    .then(res => res.json());
+    setNote(() => {
+      return note.filter((item) => {
+            return item._id !== data._id
+      })
+    })
+
+  }
+  async function createNote(titleText, contentText){
+    
+    const data = await fetch(API_BASE + "/note", {
+      method:"POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        title: titleText,
+        content: contentText
+  })})
+    .then(res => res.json());
+    setNote((prev) => {
+      return [
+        ...prev,
+        data
+      ];
+    });
+
+  }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+  // function addNote(titleText, contentText) {
+  //   setNote((prev) => {
+  //     return [
+  //       ...prev,
+  //       {
+  //         title: titleText,
+  //         content: contentText
+  //       }
+  //     ];
+  //   });
+  // }
+  // function removeNote(id) {
+  //   setNote(
+  //     note.filter((item, index) => {
+  //       return index !== id;
+  //     })
+  //   );
+  // }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea addNote={createNote}  />
+      {note.map((item, index) => {
+        return (
+          <Note
+            key={index}
+            id={item._id}
+            title={item.title}
+            content={item.content}
+            removeItem={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+function CreateArea(props) {
+  const [valid, setValid] = useState(true);
+  const [inputText, setInputText] = useState("");
+  const [textAreaText, setTextAreaText] = useState("");
+  function handleChange(e) {
+    var { value, name } = e.target;
+    if (name === "title") {
+      setInputText(value);
+    } else if (name === "content") {
+      setTextAreaText(value);
+    }
+  }
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          
+          if (inputText === "" || textAreaText === "") {
+              setValid(false)
+              
+          } else {
+            setValid(true)
+            props.addNote(inputText, textAreaText);
+            setInputText("");
+            setTextAreaText("");
+          }
+        }}
+      >
+        {valid ? null : <p>Please enter a valid note.</p>}
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          value={inputText}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleChange}
+          value={textAreaText}
+        />
+        <button type="submit">
+          <AddIcon />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
